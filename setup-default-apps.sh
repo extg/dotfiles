f4d4d52e50e977c05940b99c07a9dfc49d24ab3d
extg <extg@users.noreply.github.com>
2024-12-23 00:14:03 +0000

    Upd default apps

--- setup-default-apps.sh
+++ setup-default-apps.sh
@@ -1,32 +1,43 @@
 #!/usr/bin/env bash
 
-duti -s com.microsoft.VSCode .c all
-duti -s com.microsoft.VSCode .cpp all
-duti -s com.microsoft.VSCode .cs all
-duti -s com.microsoft.VSCode .css all
-duti -s com.microsoft.VSCode .go all
-duti -s com.microsoft.VSCode .java all
-duti -s com.microsoft.VSCode .js all
-duti -s com.microsoft.VSCode .jsx all
-duti -s com.microsoft.VSCode .ts all
-duti -s com.microsoft.VSCode .tsx all
-duti -s com.microsoft.VSCode .sass all
-duti -s com.microsoft.VSCode .scss all
-duti -s com.microsoft.VSCode .less all
-duti -s com.microsoft.VSCode .vue all
-duti -s com.microsoft.VSCode .cfg all
-duti -s com.microsoft.VSCode .json all
-duti -s com.microsoft.VSCode .lua all
-duti -s com.microsoft.VSCode .md all
-duti -s com.microsoft.VSCode .php all
-duti -s com.microsoft.VSCode .pl all
-duti -s com.microsoft.VSCode .py all
-duti -s com.microsoft.VSCode .rb all
-duti -s com.microsoft.VSCode .rs all
-duti -s com.microsoft.VSCode .sh all
-duti -s com.microsoft.VSCode .swift all
-duti -s com.microsoft.VSCode .txt all
-duti -s com.microsoft.VSCode .conf all
-duti -s com.microsoft.VSCode .yaml all
-duti -s com.microsoft.VSCode .yml all
-duti -s com.microsoft.VSCode .toml all
+# To determine the bundle ID of the Cursor application on macOS, 
+# run the following command:
+# mdls -name kMDItemCFBundleIdentifier /Applications/Cursor.app
+#
+# > com.todesktop.230313mzl4w4u92
+
+duti -s com.todesktop.230313mzl4w4u92 .c all
+duti -s com.todesktop.230313mzl4w4u92 .cpp all
+duti -s com.todesktop.230313mzl4w4u92 .cs all
+duti -s com.todesktop.230313mzl4w4u92 .css all
+duti -s com.todesktop.230313mzl4w4u92 .go all
+duti -s com.todesktop.230313mzl4w4u92 .java all
+duti -s com.todesktop.230313mzl4w4u92 .js all
+duti -s com.todesktop.230313mzl4w4u92 .jsx all
+duti -s com.todesktop.230313mzl4w4u92 .ts all
+duti -s com.todesktop.230313mzl4w4u92 .tsx all
+duti -s com.todesktop.230313mzl4w4u92 .sass all
+duti -s com.todesktop.230313mzl4w4u92 .scss all
+duti -s com.todesktop.230313mzl4w4u92 .less all
+duti -s com.todesktop.230313mzl4w4u92 .vue all
+duti -s com.todesktop.230313mzl4w4u92 .cfg all
+duti -s com.todesktop.230313mzl4w4u92 .json all
+duti -s com.todesktop.230313mzl4w4u92 .lua all
+duti -s com.todesktop.230313mzl4w4u92 .md all
+duti -s com.todesktop.230313mzl4w4u92 .php all
+duti -s com.todesktop.230313mzl4w4u92 .pl all
+duti -s com.todesktop.230313mzl4w4u92 .py all
+duti -s com.todesktop.230313mzl4w4u92 .rb all
+duti -s com.todesktop.230313mzl4w4u92 .rs all
+duti -s com.todesktop.230313mzl4w4u92 .sh all
+duti -s com.todesktop.230313mzl4w4u92 .swift all
+duti -s com.todesktop.230313mzl4w4u92 .txt all
+duti -s com.todesktop.230313mzl4w4u92 .conf all
+duti -s com.todesktop.230313mzl4w4u92 .yaml all
+duti -s com.todesktop.230313mzl4w4u92 .yml all
+duti -s com.todesktop.230313mzl4w4u92 .toml all
+duti -s com.todesktop.230313mzl4w4u92 .md all
+duti -s com.todesktop.230313mzl4w4u92 .xml all
+duti -s com.todesktop.230313mzl4w4u92 .sql all
+duti -s com.todesktop.230313mzl4w4u92 .astro all
+duti -s com.todesktop.230313mzl4w4u92 .mdx all
